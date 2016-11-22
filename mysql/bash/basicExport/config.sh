fee1630ddc93061ba5dec3cf0bfbdae9e7cda703
leovujanic <leonard.vujanic@gmail.com>
2016-11-22 10:40:55 +0100

    Update

--- mysql/bash/basicExport/config.sh
+++ mysql/bash/basicExport/config.sh
@@ -8,7 +8,7 @@ DBPASS=""
 
 # databases to be exported use "*" if you want backup all DB-s
 # for specific databases use "db1, db2,db3"
-DATABASES="*"
+DATABASES="leadership-academy"
 
 # Destination directory
 DUMP_DESTINATION="/Users/leonardvujanic/mysqlDump/dumps"
